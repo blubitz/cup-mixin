4041ace915990222ba065d8da9c2eae822a03b0c
blubitz <20598803+blubitz@users.noreply.github.com>
2025-03-22 16:00:34 +0800

    create npm package and tests

---
+++ index.js
@@ -0,0 +1,35 @@
+export class Mixin {
+  static mixin(target) {
+    const mixinName = `_${this.name.toLowerCase()}_mixin`
+    target[mixinName] = new this()
+
+    function addGetterSetter(parent, instance, name, descriptor) {
+      Object.defineProperty(parent, name, {
+        get: descriptor.get ? function () { return instance[name]; } : undefined,
+        set: descriptor.set ? function (value) { instance[name] = value; } : undefined,
+        configurable: true,
+        enumerable: true,
+      });
+    }
+
+    // Expose public methods dynamically
+    for (const key of Object.getOwnPropertyNames(this.prototype)) {
+      if (key !== "constructor" && !key.startsWith("#")) {
+        const descriptor = Object.getOwnPropertyDescriptor(this.prototype, key);
+
+        if (descriptor.get || descriptor.set) {
+          addGetterSetter(target, target[mixinName], key, descriptor);
+        } else {
+          target[key] = function (...args) {
+            return target[mixinName][key](...args);
+          };
+        }
+      }
+    }
+
+    // Expose public variables dynamically
+    for (const key of Object.keys(target[mixinName])) {
+      target[key] = target[mixinName][key]
+    }
+  }
+}
---
+++ test/test.js
@@ -0,0 +1,63 @@
+import { Mixin } from '../index.js'
+import assert from 'assert';
+
+class Test extends Mixin {
+  #privateVar = 12
+
+  constructor() {
+    super()
+    this.publicVar = 9
+  }
+
+  #privateMethod() {
+    return 'private'
+  }
+
+  publicMethod() {
+    return 'public'
+  }
+
+  privateMethod() {
+    return this.#privateMethod()
+  }
+
+  privateVar() {
+    return this.#privateVar
+  }
+
+  get val() {
+    return this.#privateVar + 1
+  }
+
+  set val(num) {
+    this.#privateVar = num
+  }
+}
+
+class MyClass {
+  constructor() {
+    Test.mixin(this); // Auto-attach mixin with dynamic name
+  }
+}
+
+const obj = new MyClass();
+
+it('should mix in public methods', () => {
+  assert.equal(obj.publicMethod(), 'public')
+})
+it('should mix in private methods', () => {
+  assert.equal(obj.privateMethod(), 'private')
+})
+it('should mix in public variables', () => {
+  assert.equal(obj.publicVar, 9)
+})
+it('should mix in private variables', () => {
+  assert.equal(obj.privateVar(), 12)
+})
+it('should mix in getters', () => {
+  assert.equal(obj.val, 13)
+})
+it('should mix in setters', () => {
+  obj.val = 42
+  assert.equal(obj.privateVar(), 42)
+})
